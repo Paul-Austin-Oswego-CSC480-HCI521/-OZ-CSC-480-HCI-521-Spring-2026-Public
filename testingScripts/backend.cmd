89f2bcebfe8ec7f00b6b9d90d03a7a083499bfa9
Gooseclemons <scottwilmot04@gmail.com>
2026-02-20 02:08:31 -0500

    Simple Windows scripts for starting the different components for testing purposes (backend, frontend, database)

--- testingScripts/backend.cmd
+++ testingScripts/backend.cmd
@@ -1,5 +1,5 @@
 cd ..
 cd ./backend/finish
-.\mvnw.cmd liberty:dev
+.\mvnw.cmd liberty%:dev
 
 :: Run liberty:stop instead of liberty:dev if you are getting errors for a live liberty instance blocking start
